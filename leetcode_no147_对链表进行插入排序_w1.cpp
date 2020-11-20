ef17e49ac2e790ff02d332099fad99f69891851d
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-11-20 23:48:33 +0800

    Add files via upload

---
+++ leetcode_no147_对链表进行插入排序_w1.cpp
@@ -0,0 +1,95 @@
+﻿/* ------------------------------------------------------------------|
+对链表进行插入排序。
+
+
+插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+
+ 
+
+插入排序算法：
+
+插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+重复直到所有输入数据插入完为止。
+ 
+
+示例 1：
+
+输入: 4->2->1->3
+输出: 1->2->3->4
+示例 2：
+
+输入: -1->5->3->4->0
+输出: -1->0->3->4->5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/insertion-sort-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.1 MB, 在所有 C++ 提交中击败了81.94%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* insertionSortList(ListNode* head) {
+    if (!head) {
+        return nullptr;
+    }
+    //开一个新头节点进行链接操作，不在原有的链表上修改
+    ListNode* dummyHead = new ListNode(0);
+    //dummyHead->next = head;
+    ListNode* now = head;
+    ListNode* pre = dummyHead;
+
+    while (now) {
+        while (pre->next && now->val > pre->next->val) {
+            pre = pre->next;
+        }
+        ListNode* next = now->next; 
+        now->next = pre->next;
+        pre->next = now;
+        //因为now前面的节点指向了now，想让前一个节点指向now的next就直接将now修改为next即可
+        now = next;
+        pre = dummyHead;
+    }
+    return dummyHead->next;
+}
+
+int main() {
+    ListNode* head = new ListNode(4);
+    ListNode* n1 = new ListNode(2);
+    ListNode* n2 = new ListNode(1);
+    ListNode* n3 = new ListNode(3);
+    head->next = n1;
+    n1->next = n2;
+    n2->next = n3;
+
+    ListNode* res = insertionSortList(head);
+}
+
